538113c0ac940368bfd3f4d574f2e672deccd01f
xuanson501123 <xuanson501123@users.noreply.github.com>
2025-11-04 08:37:34 +0000

    Cập nhật UDID: 021BA867-7EEB-4D9C-9717-DFD9BC87E999

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,17 @@
+# keep_alive.py
 from flask import Flask
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask("")
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Bot đang hoạt động!"
+    return "Bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
